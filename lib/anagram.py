6382df6712ef3517adfbceced81498517694159d
gift kimani <gift.kimani@students.moringaschool.com>
2023-08-31 01:52:59 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+
+        for word in words:
+            is_anagram  = True
+            for c in word:
+                if not (c in self.word):
+                    is_anagram = False
+                    break
+                    
+
+            if is_anagram:
+                anagrams.append(word)
+
+        return anagrams
